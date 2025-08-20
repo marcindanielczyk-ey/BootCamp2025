a7759837283f9a6dc6ebb078c10ca9b89c9d3cf2
Marcin <marcin.danielczyk@gds.ey.com>
2025-08-20 12:36:32 +0200

    featurebranchC first commit

--- src/GitVersioningClass.java
+++ src/GitVersioningClass.java
@@ -2,5 +2,9 @@ public class GitVersioningClass {
     public static void main(String[] args) {
         System.out.println("Git versioning exercise");
         System.out.println("Line for git commit -amend");
+
+        String name = "Marcin";
+
+        System.out.println("First commit for featurebranchc made by " + name);
     }
 }
