4f8676b56f657deb6738eec160f255eba3438cda
Marcin <marcin.danielczyk@gds.ey.com>
2025-08-20 12:33:28 +0200

    simple change for featurebranchb first commit

--- src/GitVersioningClass.java
+++ src/GitVersioningClass.java
@@ -2,5 +2,7 @@ public class GitVersioningClass {
     public static void main(String[] args) {
         System.out.println("Git versioning exercise");
         System.out.println("Line for git commit -amend");
+
+        System.out.println("First commit FeatureBranchB");
     }
 }
