be858b2af96b90503f73ccdfe83a3cf9de89accf
Marcin <marcin.danielczyk@gds.ey.com>
2025-08-28 19:43:43 +0200

    Lambda expressions second exercise

---
+++ src/lambdaexpressions/exercise2/Employee.java
@@ -0,0 +1,24 @@
+package lambdaexpressions.exercise2;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
